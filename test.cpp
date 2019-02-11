205b7c173f1f7da911b2918802ae613c6877e8c5
R. G. Esteves <rgesteves@gmail.com>
2019-02-10 22:40:12 -0800

    Added basic tests

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+#include "sum_integers.hpp"
+
+int main() {
+  auto integers = {1, 2, 3, 4, 5};
+
+  if (sum_integers(integers) == 15) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
